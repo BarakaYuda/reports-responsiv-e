c38104fe4872bcea4d209c8d710477985d082d12
baraka_yudah <114602421+BarakaYuda@users.noreply.github.com>
2022-10-12 18:17:43 +0300

    retrived loading

--- reports/report.php
+++ reports/report.php
@@ -14,8 +14,39 @@ if(!$con){
 $sql = 'SELECT * FROM schedule2';
 
 $result = mysqli_query($con,$sql);
- 
-
-
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+
+</head>
+<body>
+   <table aling="center" border="1px" style="width:600px; line: height 40px;">
+      <tr><h1>patient records</h1></tr>
+      <t class="table">
+           <th>book_no</th>
+           <th>Doctor</th>
+           <th>Email</th>
+           <th>Dates</th>
+           <th>mobile</th>
+           <th>full_name</th>
+      </t>
+      <?php
+        while ( $row= mysql_fetch_array($tables))
+
+?>
+<tr>
+   <td><?php echo $table['book_no']; ?></td>
+   <td><?php echo $table['Doctors']; ?></td>
+   <td><?php echo $table['Email']; ?></td>
+   <td><?php echo $table['Dates']; ?></td>
+   <td><?php echo $table['mobile']; ?></td>
+   <td><?php echo $table['Full_name']; ?></td>
+   </table>
+</body>
+</html>
